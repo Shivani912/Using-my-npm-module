d742b70a81cd33140ee45ee8c870e165ccd03c43
Shivani Joshi <46731446+Shivani912@users.noreply.github.com>
2019-07-30 14:33:52 -0500

    Create usingSimpleCal.js

---
+++ usingSimpleCal.js
@@ -0,0 +1,6 @@
+const sc = require('simplecal');
+
+console.log("add : "+sc.add(1,2));
+console.log("subtract : "+sc.subtract(1,2));
+console.log("multiply : "+sc.multiply(1,2));
+console.log("divide : "+sc.divide(1,2));
